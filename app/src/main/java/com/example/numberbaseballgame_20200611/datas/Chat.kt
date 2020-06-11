5b37aa396a2b318261e32964e090f973541d3786
meltingshine <meltingshine@gmail.com>
2020-06-11 20:16:58 +0900

    채팅 한줄 XML 그리기인데 XML하나 그려놓고 KT에다가 visibility활용해서 한개씩 써먹기 올

--- app/src/main/java/com/example/numberbaseballgame_20200611/datas/Chat.kt
+++ app/src/main/java/com/example/numberbaseballgame_20200611/datas/Chat.kt
@@ -4,5 +4,7 @@ package com.example.numberbaseballgame_20200611.datas
 //Chat은 누가/뭐라고
 //who - USER / CPU
 //content 내용
-class Chat(val who:String, val content : String){
+class Chat(
+    val who:String,
+    val content : String){
 }
