53c47b53da97d25609a52407fbc70863afef8e4d
meltingshine <meltingshine@gmail.com>
2020-06-11 19:59:29 +0900

    뽑아낸 숫자가 맞는지 검사하는것도 짬 올

--- app/src/main/java/com/example/numberbaseballgame_20200611/MainActivity.kt
+++ app/src/main/java/com/example/numberbaseballgame_20200611/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.numberbaseballgame_20200611
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : BaseActivity() {
     // 컴퓨터가 낸 문제 숫자 3개를 저장할 ArrayList
@@ -32,20 +33,33 @@ class MainActivity : BaseActivity() {
 
             //규칙에 맞는 숫자를 뽑을때까지 무한 반복
             while (true) {
-//                TODO 1에서 9의 숫자를 랜덤으로 뽑음
+
 
                 //랜덤은 0<= x <1
                 //한다음에 .toInt() 하면 소수점 밑에 버려벌임
                 val randomNum = (Math.random()*9+1).toInt()
 
+                Log.d("랜덤값",randomNum.toString())
+
 
                 // 뽑은 숫자를 사용해도 될지를 저장하는 변수
                 var isNumberOk = true
 
-                //TODO 뽑은 숫자를 써도 되는지 검사 로직 => isNumberOk 의 값 변경
+                //중복이 아니어야 사용해도 좋은 숫자로 인정. (1-9는 위에서 셋함)
+                //중복 검사 : 문제 숫자 배열에 있는 값들을 다 꺼내서 지금 만든 값과 비교해봄
+
+                for(computerHadNumber in computerNumbers) {
+
+//                    같으면 중복임 사용하면 안됨
+                    if (computerHadNumber == randomNum) {
+                        isNumberOk = false
+                        break //for문 나가기
+                    }
+
+                }
 
                 //조건에 맞는 숫자를 뽑으면 무한 반복 끝내기
-                if (isNumberOk) break
+                if (isNumberOk) {break}
             }
 
         }
@@ -53,4 +67,4 @@ class MainActivity : BaseActivity() {
     }
 
 }
-}
+
